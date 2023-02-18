2180e900b381daf604e346b9318779f0b9fc2ee9
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 17:25:27 +0900

    5장 예제코드 추가 - 5.6.1

--- src/main/java/me/shinsunyoung/musthavespringboot/service/BlogService.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/service/BlogService.java
@@ -29,4 +29,8 @@ public class BlogService {
                 .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
     }
 
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
 }
