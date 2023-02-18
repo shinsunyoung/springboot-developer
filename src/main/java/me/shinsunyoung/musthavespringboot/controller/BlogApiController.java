78b7d44f1baf51c89f0772a0c88c2167ef0e564d
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 16:58:40 +0900

    5장 예제코드 추가 - 5.4.2

--- src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogApiController.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.shinsunyoung.musthavespringboot.controller;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.musthavespringboot.domain.Article;
 import me.shinsunyoung.musthavespringboot.dto.AddArticleRequest;
+import me.shinsunyoung.musthavespringboot.dto.ArticleResponse;
 import me.shinsunyoung.musthavespringboot.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +26,17 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
 
