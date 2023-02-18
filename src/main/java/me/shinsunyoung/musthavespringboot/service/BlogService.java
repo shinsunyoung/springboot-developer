c536e40eb2ca0fa8f461ca430d6d71f60ed05f4e
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 16:58:25 +0900

    5장 예제코드 추가 - 5.4.1

--- src/main/java/me/shinsunyoung/musthavespringboot/service/BlogService.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/service/BlogService.java
@@ -3,8 +3,12 @@ package me.shinsunyoung.musthavespringboot.service;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.musthavespringboot.domain.Article;
 import me.shinsunyoung.musthavespringboot.dto.AddArticleRequest;
+import me.shinsunyoung.musthavespringboot.dto.ArticleResponse;
 import me.shinsunyoung.musthavespringboot.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +18,9 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
