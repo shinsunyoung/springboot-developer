cdab1e4f6a54738dea7d6e4b8674944457b343da
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 17:26:11 +0900

    5장 예제코드 추가 - 5.6.2

--- src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogApiController.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogApiController.java
@@ -42,5 +42,13 @@ public class BlogApiController {
         return ResponseEntity.ok()
                 .body(new ArticleResponse(article));
     }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
 }
 
