a8040449edf1c78092479f75a1164ea7d50bd8a0
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 15:33:43 +0900

    5장 예제코드 추가 - 5.3.2

---
+++ src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogApiController.java
@@ -0,0 +1,27 @@
+package me.shinsunyoung.musthavespringboot.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.musthavespringboot.domain.Article;
+import me.shinsunyoung.musthavespringboot.dto.AddArticleRequest;
+import me.shinsunyoung.musthavespringboot.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
+
