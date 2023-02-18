69438af87239ac6d9720089e3bb1825c70346e80
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 15:01:03 +0900

    3장 예제코드 추가 - 3.3

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -14,14 +14,14 @@ public class JUnitTest {
         Assertions.assertEquals(a + b, sum);
     }
 
-    @DisplayName("1 + 3는 4이다")
-    @Test
-    public void junitFailedTest() {
-        int a = 1;
-        int b = 3;
-        int sum = 3;
-
-        Assertions.assertEquals(a + b, sum);
-    }
+//    @DisplayName("1 + 3는 4이다")
+//    @Test
+//    public void junitFailedTest() {
+//        int a = 1;
+//        int b = 3;
+//        int sum = 3;
+//
+//        Assertions.assertEquals(a + b, sum);
+//    }
 }
 
