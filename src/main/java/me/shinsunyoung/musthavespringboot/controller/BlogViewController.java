5de65006d1d80d424534ed7bfe682870077a2dd7
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 18:55:20 +0900

    6장 예제코드 추가 - 6.3.2

--- src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogViewController.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.shinsunyoung.musthavespringboot.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.musthavespringboot.domain.Article;
 import me.shinsunyoung.musthavespringboot.dto.ArticleListViewResponse;
+import me.shinsunyoung.musthavespringboot.dto.ArticleViewResponse;
 import me.shinsunyoung.musthavespringboot.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,4 +28,13 @@ public class BlogViewController {
 
         return "articleList";
     }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
 }
