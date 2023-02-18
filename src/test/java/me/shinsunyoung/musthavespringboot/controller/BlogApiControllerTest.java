6f865514a462759e45871f709494c1f5c03032fc
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 17:01:47 +0900

    5장 예제코드 추가 - 5.4.4

--- src/test/java/me/shinsunyoung/musthavespringboot/controller/BlogApiControllerTest.java
+++ src/test/java/me/shinsunyoung/musthavespringboot/controller/BlogApiControllerTest.java
@@ -72,5 +72,30 @@ class BlogApiControllerTest {
         assertThat(articles.get(0).getTitle()).isEqualTo(title);
         assertThat(articles.get(0).getContent()).isEqualTo(content);
     }
+
+    @DisplayName("findAllArticles: 아티클 목록 조회에 성공한다.")
+    @Test
+    public void findAllArticles() throws Exception {
+        // given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
+    }
+
 }
 
