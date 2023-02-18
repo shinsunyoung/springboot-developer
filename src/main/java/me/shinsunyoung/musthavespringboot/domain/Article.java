eef91617c5e2fedec8af203fd0a8e1a12d8795ac
Sunyoung Shin <ajufresh@gmail.com>
2023-02-18 18:54:18 +0900

    6장 예제코드 추가 - 6.3.1

--- src/main/java/me/shinsunyoung/musthavespringboot/domain/Article.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/domain/Article.java
@@ -5,6 +5,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -21,6 +25,14 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder
     public Article(String title, String content) {
