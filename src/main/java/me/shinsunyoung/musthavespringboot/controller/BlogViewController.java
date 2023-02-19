fe29b252f4b75b3abd77a1f6060b7c6106c2bb7a
Sunyoung Shin <ajufresh@gmail.com>
2023-02-19 19:56:54 +0900

    6장 예제코드 추가 - 6.5.2

--- src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogViewController.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -37,4 +38,16 @@ public class BlogViewController {
         return "article";
     }
 
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
 }
--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -13,3 +13,27 @@ if (deleteButton != null) {
             });
     });
 }
+
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton != null) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch('/api/articles/' + id, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+            .then((response) => {
+                alert('수정이 완료되었습니다.');
+                location.replace('/articles/' + id);
+            });
+    });
+}
