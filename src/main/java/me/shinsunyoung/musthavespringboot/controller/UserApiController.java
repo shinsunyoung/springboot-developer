df25c7d41c8ac123867dd2165781160657ef0e65
Sunyoung Shin <ajufresh@gmail.com>
2023-02-19 23:50:45 +0900

    7장 예제코드 추가 - 7.6.1

--- src/main/java/me/shinsunyoung/musthavespringboot/controller/UserApiController.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.shinsunyoung.musthavespringboot.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.musthavespringboot.dto.AddUserRequest;
 import me.shinsunyoung.musthavespringboot.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -17,4 +22,11 @@ public class UserApiController {
         userService.save(request);
         return "redirect:/login";
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
+    }
+
 }
