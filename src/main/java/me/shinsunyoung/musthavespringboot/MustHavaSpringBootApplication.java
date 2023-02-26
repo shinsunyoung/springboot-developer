91596a9b4ba2fd071d80f26215606cba323ae2fd
shinsunyoung <ajufresh@gmail.com>
2023-02-27 01:38:01 +0900

    6장 예제코드 보강 - 6.3.1

--- src/main/java/me/shinsunyoung/musthavespringboot/MustHavaSpringBootApplication.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/MustHavaSpringBootApplication.java
@@ -2,7 +2,9 @@ package me.shinsunyoung.musthavespringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MustHavaSpringBootApplication {
     public static void main(String[] args) {
