369f319bff55e284270e382246cbd1c170e9b6c2
shinsunyoung <ajufresh@gmail.com>
2023-02-26 22:15:52 +0900

    9장 예제코드 추가 - 9.3.3

--- src/main/java/me/shinsunyoung/musthavespringboot/domain/User.java
+++ src/main/java/me/shinsunyoung/musthavespringboot/domain/User.java
@@ -26,14 +26,25 @@ public class User implements UserDetails {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
+
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
+    }
+
 
 
     @Override
