58922e731890e7f0afd83c584c519e365315cc71
shinsunyoung <ajufresh@gmail.com>
2023-04-09 13:46:40 +0900

    fix: 5장 예제코드 수정

--- chapter5/src/main/resources/data.sql
+++ chapter5/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article (id, title, content) VALUES (1, '제목1', '내용1')
-INSERT INTO article (id, title, content) VALUES (2, '제목2', '내용2')
-INSERT INTO article (id, title, content) VALUES (3, '제목3', '내용3')
+INSERT INTO article (title, content) VALUES ('제목1', '내용1')
+INSERT INTO article (title, content) VALUES ('제목2', '내용2')
+INSERT INTO article (title, content) VALUES ('제목3', '내용3')
